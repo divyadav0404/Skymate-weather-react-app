ec5090d1f24b59c7f21a79a81be7e6b7d32e3833
divyadav0404 <divyanshyadav206@gmail.com>
2025-08-03 17:31:19 +0530

    Initial commit with deploy setup

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+   base: "/skymate-weather-react-app/",  // 👈 this line is the key!
   plugins: [react()],
 })
